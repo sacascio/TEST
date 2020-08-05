350384279fb1f2e995cf887f8435edbe47e794e9
sacascio <sacascio@cisco.com>
2020-08-05 00:07:40 -0400

    Updated on Aug 5

--- hello_world.py
+++ hello_world.py
@@ -3,13 +3,13 @@ import sys
 # comment
 # Newer comment
 # part of hotfix
-
+# Updated August 5th, 2020
 
 def hello():
 
-	print ("Hello")
+	print("Hello")
 
 print ("Hello World")
-
 print("I am exiting")
+print ("updated Aug 5")
 sys.exit(9)
